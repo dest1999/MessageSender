cc8beda43d407c0196823d4c9c4a5eea6b8c1ebf
Oleg <serv_all@mail.ru>
2022-05-16 13:58:38 +0300

    Введены атрибуты валидации

--- MessageSender/Models/AbstractPerson.cs
+++ MessageSender/Models/AbstractPerson.cs
@@ -1,10 +1,21 @@
-﻿namespace MessageSender;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MessageSender;
 
 public abstract class AbstractPerson
 {
     public int Id { get; set; }
+    
+    [Required]
+    [StringLength(100)]
     public string Name { get; set; }
+
+    [Required]
+    [EmailAddress]
     public string EMail { get; set; }
+    
+    [Phone]
     public string Phone { get; set; }
+
     public abstract void CopyDataFrom(AbstractPerson abstractPerson);
 }
