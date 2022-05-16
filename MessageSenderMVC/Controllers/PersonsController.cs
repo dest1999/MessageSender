5a5524871a2de2fd4e92cf6d7fb9bdfc27883fa3
Oleg <serv_all@mail.ru>
2022-05-16 17:07:48 +0300

    Добавлена страница Persons с отображением из тестового репозитория. Добавление в репозиторий работает

---
+++ MessageSenderMVC/Controllers/PersonsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MessageSenderMVC.Controllers
+{
+    public class PersonsController : Controller
+    {
+        private IRepository<Person> personRepository;
+
+        public PersonsController(IRepository<Person> PersonRepository)
+        {
+            personRepository = PersonRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var tmp = personRepository.GetAll();
+            //var tmp = personRepository.ReadById(1);
+            return View(tmp);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new PersonViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Create(PersonViewModel personViewModel)
+        {
+            var person = new Person();
+            person.CopyDataFrom(personViewModel);
+            personRepository.Create(person);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- MessageSenderMVC/Program.cs
+++ MessageSenderMVC/Program.cs
@@ -1,7 +1,10 @@
+using MessageSenderMVC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IRepository<Person>, InRamRepo>();
 
 var app = builder.Build();
 
