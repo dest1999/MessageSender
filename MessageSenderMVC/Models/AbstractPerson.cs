17a6deb629c240bf7b5e09af490d7a4866a0f0bc
Oleg <serv_all@mail.ru>
2022-05-16 16:12:44 +0300

    Создан проект MVC, репозиторий и модели скопированы в него

---
+++ MessageSenderMVC/Models/AbstractPerson.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MessageSenderMVC;
+
+public abstract class AbstractPerson
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string EMail { get; set; }
+    
+    [Phone]
+    public string Phone { get; set; }
+
+    public abstract void CopyDataFrom(AbstractPerson abstractPerson);
+}
---
+++ MessageSenderMVC/Repository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace MessageSenderMVC;
+
+public interface IRepository<T> where T : Person
+{
+    void Create(T person);
+    T? ReadById(int id);
+    void Update(T person);
+    void Delete(int id);
+    IEnumerable<T> GetAll();
+}
